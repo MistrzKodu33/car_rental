8d7cb9e79d0f312abc8f39cdf0c2fdc0c27f6b9f
MistrzKodu <mistrzkodu@hotmail.com>
2024-11-21 18:21:08 +0100

    Create projects

---
+++ Projects/api/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    public class TestController : _MyApiController
+    {
+        public string Index()
+        {
+            return "Test api";
+        }
+    }
+}
---
+++ Projects/api/Controllers/_MyApiController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class _MyApiController : ControllerBase
+    {
+        public _MyApiController()
+        {
+
+        }
+    }
+}
